3a803863f95ac863fab54ddc143515989e66168c
rebeca <beca.cout@gmail.com>
2023-11-29 13:50:40 -0300

    chore: config routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import "./App.css";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline } from "@mui/material";
 import Topbar from "./components/global/Topbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Pie from "./pages/Pie";
+import Form from "./pages/Form";
+import Calendar from "./pages/Calendar";
+import Line from "./pages/Line";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -14,6 +20,15 @@ function App() {
         <div className="app flex relative">
           <main className="content">
             <Topbar />
+            <Router>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/pie-chart" element={<Pie />} />
+                <Route path="/pie-chart" element={<Line />} />
+              </Routes>
+            </Router>
           </main>
         </div>
       </ThemeProvider>
