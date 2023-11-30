9fece4a1126c2e0de0665f8b8067712b8df35265
rebeca <beca.cout@gmail.com>
2023-11-30 13:25:52 -0300

    initial form doctor

--- src/pages/doctor/Form.jsx
+++ src/pages/doctor/Form.jsx
@@ -1,19 +1,163 @@
-import { Box, useMediaQuery } from "@mui/material";
-import Header from "../../components/global/Header";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import { Formik } from "formik";
+import * as yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../../components/global/Header";
 
 const Form = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
+
   const handleFormSubmit = (values) => {
     console.log(values);
   };
 
   return (
     <Box m="20px">
-      <Header title="Create Doctor" subtitle="Insert a new doctor in system" />
-      <Formik></Formik>
+      <Header title="Create Doctor" subtitle="Create a New Doctor in System" />
+
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={checkoutSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+              sx={{
+                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+              }}
+            >
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Doctor Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.name}
+                name="name"
+                error={!!touched.name && !!errors.name}
+                helperText={touched.name && errors.name}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="CRM"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.crm}
+                name="crm"
+                error={!!touched.crm && !!errors.crm}
+                helperText={touched.crm && errors.crm}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                error={!!touched.email && !!errors.email}
+                helperText={touched.email && errors.email}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Phone Number"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.phone}
+                name="phone"
+                error={!!touched.phone && !!errors.phone}
+                helperText={touched.phone && errors.phone}
+                sx={{ gridColumn: "span 2" }}
+              />
+
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Street"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.street}
+                name="street"
+                error={!!touched.street && !!errors.street}
+                helperText={touched.street && errors.street}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Address 2"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.number}
+                name="number"
+                error={!!touched.number && !!errors.number}
+                helperText={touched.number && errors.number}
+                sx={{ gridColumn: "span 2" }}
+              />
+            </Box>
+            <Box display="flex" justifyContent="end" mt="20px">
+              <Button type="submit" color="secondary" variant="contained">
+                Create New User
+              </Button>
+            </Box>
+          </form>
+        )}
+      </Formik>
     </Box>
   );
+};
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const checkoutSchema = yup.object().shape({
+  name: yup.string().required("required"),
+  crm: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  phone: yup
+    .string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("required"),
+  street: yup.string().required("required"),
+  number: yup.string().required("required"),
+});
+const initialValues = {
+  name: "",
+  crm: "",
+  email: "",
+  phone: "",
+  speciality: "",
+  address: {
+    street: "",
+    number: "",
+    complement: "",
+    neighborhood: "",
+    city: "",
+    state: "",
+    postalCode: "",
+  },
 };
 
 export default Form;
